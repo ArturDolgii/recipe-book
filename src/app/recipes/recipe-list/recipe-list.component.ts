88e53d1aeaa5d527af27b91e951ab274afea0a8c
Artur Dolhii <artur.dolhii@medhost.com>
2021-03-10 21:35:05 +0200

    added components and layout

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is simple description',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    ),
+    new Recipe('A Test Recipe 2',
+      'This is simple description 2',
+      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'
+    )
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
